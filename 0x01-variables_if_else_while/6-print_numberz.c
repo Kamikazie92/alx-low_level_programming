1076df44d906638b4321abfd119898495dce438c
Kamikazie92 <worldqosmio22@gmail.com>
2023-06-16 19:40:22 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - print 0123456789 using putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
