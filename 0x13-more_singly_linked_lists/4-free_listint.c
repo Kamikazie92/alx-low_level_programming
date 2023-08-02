902b71f576843fb63b187a9c757521aca378fe66
Kamikazie92 <worldqosmio22@gmail.com>
2023-08-01 17:14:06 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: the head of the list
+ *
+ * Return: NULL if error
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
